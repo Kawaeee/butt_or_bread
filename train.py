974d1948c55420229c7f806615f5548cbde7b238
Kasidech C <kawaekc@gmail.com>
2019-12-06 21:55:32 +0700

    Update train.py

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.optim as optimizer
 
-dataset_path = '/data/'
+dataset_path = '/data'
 
 # https://pytorch.org/docs/stable/torchvision/models.html
 
