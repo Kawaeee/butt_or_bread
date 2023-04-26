86f6b5479b9a2d6603e42822aab210305eabc55f
Kawaeee <kawaekc@gmail.com>
2023-04-26 12:24:34 +0700

    🔨 improve utils.py, including docstring

--- butt_or_bread/utils.py
+++ butt_or_bread/utils.py
@@ -1,17 +1,23 @@
 import psutil
 
 
-def health_check():
-    """Check CPU/Memory/Disk usage of deployed machine"""
+def health_check() -> str:
+    """
+    Check the CPU, memory, and disk usage of the deployed machine.
 
+    Returns:
+        A string with information about the current usage levels for each resource, formatted as follows:
+        "CPU Usage: [percent]%" | "Memory usage: [used memory]GB/[total memory]GB" | "Disk usage: [used disk]GB/[total disk]GB"
+
+    Uses the `psutil` module to obtain information about the system resources.
+    """
+    vm = psutil.virtual_memory()
+    du = psutil.disk_usage("/")
     cpu_percent = psutil.cpu_percent(0.15)
-    total_memory = psutil.virtual_memory().total / float(1 << 30)
-    used_memory = psutil.virtual_memory().used / float(1 << 30)
-    total_disk = psutil.disk_usage("/").total / float(1 << 30)
-    used_disk = psutil.disk_usage("/").used / float(1 << 30)
 
-    cpu_usage = f"CPU Usage: {cpu_percent:.2f}%"
-    memory_usage = f"Memory usage: {used_memory:,.2f}G/{total_memory:,.2f}G"
-    disk_usage = f"Disk usage: {used_disk:,.2f}G/{total_disk:,.2f}G"
+    total_memory = vm.total / 1024**3
+    used_memory = vm.used / 1024**3
+    total_disk = du.total / 1024**3
+    used_disk = du.used / 1024**3
 
-    return " | ".join([cpu_usage, memory_usage, disk_usage])
+    return f"CPU Usage: {cpu_percent:.2f}% | Memory usage: {used_memory:.2f}GB/{total_memory:.2f}GB | Disk usage: {used_disk:.2f}GB/{total_disk:.2f}GB"
